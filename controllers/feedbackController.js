be004756e13fb1497d1b12b096a4ad2d993d787d
nguyentoan06 <toan@myspa.vn>
2022-10-24 13:26:13 +0700

    [UPDATE] remove slack

--- controllers/feedbackController.js
+++ controllers/feedbackController.js
@@ -60,45 +60,45 @@ const feedbackController = {
                         feedbacks: response._id
                     }
                 })
-                axios
-                    .post(CHANNEL_FEEDBACK, {
-                        "blocks": [
-                            {
-                                "type": "divider"
-                            },
-                            {
-                                "type": "section",
-                                "text": {
-                                    "type": "mrkdwn",
-                                    "text": `*${req.body.fullname}*\n:star::star::star::star:\n\n ${req.body.body}`
-                                },
-                                "accessory": {
-                                    "type": "image",
-                                    "image_url": `${req.body.image_url}`,
-                                    "alt_text": "alt text for image"
-                                }
-                            },
-                            {
-                                "type": "divider"
-                            },
-                            {
-                                "type": "actions",
-                                "elements": [
-                                    {
-                                        "type": "button",
-                                        "text": {
-                                            "type": "plain_text",
-                                            "text": `${req.body.platform}`,
-                                            "emoji": true
-                                        },
-                                        "value": "click_me_123",
-                                        "style": "primary"
-                                    },
-                                ]
-                            }
-                        ]
-                    })
                 res.status(200).json({ status: true, data: { response } })
+                // axios
+                //     .post(CHANNEL_FEEDBACK, {
+                //         "blocks": [
+                //             {
+                //                 "type": "divider"
+                //             },
+                //             {
+                //                 "type": "section",
+                //                 "text": {
+                //                     "type": "mrkdwn",
+                //                     "text": `*${req.body.fullname}*\n:star::star::star::star:\n\n ${req.body.body}`
+                //                 },
+                //                 "accessory": {
+                //                     "type": "image",
+                //                     "image_url": `${req.body.image_url}`,
+                //                     "alt_text": "alt text for image"
+                //                 }
+                //             },
+                //             {
+                //                 "type": "divider"
+                //             },
+                //             {
+                //                 "type": "actions",
+                //                 "elements": [
+                //                     {
+                //                         "type": "button",
+                //                         "text": {
+                //                             "type": "plain_text",
+                //                             "text": `${req.body.platform}`,
+                //                             "emoji": true
+                //                         },
+                //                         "value": "click_me_123",
+                //                         "style": "primary"
+                //                     },
+                //                 ]
+                //             }
+                //         ]
+                //     })
             } catch (error) {
                 res.status(500).json({ status: false, message: "Server error" })
             }

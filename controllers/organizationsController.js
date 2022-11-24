387c6bafe0f8505dad15c5fb1592e38543b4734c
nguyentoan06 <toan@myspa.vn>
2022-11-24 09:56:52 +0700

    [UPDATE] api organizations/:id/services

--- controllers/organizationsController.js
+++ controllers/organizationsController.js
@@ -27,6 +27,26 @@ const organizationsController = {
         } catch (error) {
             res.status(500).json({ status: false, message: 'Server error' })
         }
+    },
+    getServicesByOrgId: async (req, res) => {
+        const { org_id } = req.params
+        const filter = req.query.filter ?? {}
+        const params = {
+            'filter[keyword]': filter.keyword,
+            'filter[is_momo_ecommerce_enable]': filter.is_momo_ecommerce_enable,
+            'sort': req.query.sort,
+            'include': req.query.include,
+            'page': req.query.page ?? 1,
+            'limit': req.query.limit ?? 15,
+            'append': req.query.append
+        }
+        let paramsURL = `?${new URLSearchParams(pickBy(params, identity)).toString()}`
+        try {
+            const resServices = await axios.get(`${API_PAR}/v1/organizations/${org_id}/services${paramsURL}`)
+            res.status(200).json({ data: resServices.data })
+        } catch (error) {
+            res.status(500).json({ status: false, message: 'Server error' })
+        }
     }
 }
 module.exports = organizationsController
--- router/organizationsRoute.js
+++ router/organizationsRoute.js
@@ -2,5 +2,6 @@ const route = require('express').Router()
 const organizationsController = require('../controllers/organizationsController')
 
 route.get('/', organizationsController.get)
+route.get('/:org_id/services', organizationsController.getServicesByOrgId)
 
 module.exports = route

9efe7b7c7c9a33de1cea1bfd4cb6f39a44c5921b
nguyentoan06 <toan@myspa.vn>
2022-12-07 15:51:25 +0700

    [UPDATE] api refresh trends comment, get comments by trend

--- router/tiktokRoute.js
+++ router/tiktokRoute.js
@@ -1,9 +1,7 @@
 const route = require("express").Router();
 const tiktokController = require("../controllers/tiktokController")
 
-// route.get("/getVideoByUrl", tiktokController.getVideoByUrl);
 route.get('/getCommentsByUrl', tiktokController.getCommentsByUrl)
 route.get('/refresh_comment/:id', tiktokController.refreshComment)
-// route.get('/delete', tiktokController.delete)
 
 module.exports = route

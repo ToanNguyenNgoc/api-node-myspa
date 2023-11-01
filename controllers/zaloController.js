5a0e75f6a40442422308596f1c6781de534aab7f
Nguyen Ngoc Toan <ngoctoan06011998@gmail.com>
2023-11-01 14:28:01 +0700

    fix send noti

--- controllers/zaloController.js
+++ controllers/zaloController.js
@@ -4,6 +4,8 @@ const zaloController = {
   sendNotification: async (req, res) => {
     try {
       const body = req.body
+      const MINI_APP_ID = req.body
+      if(!MINI_APP_ID) return res.status(400).json({ message: 'Mini app id is required' })
       if (!body.zalo_user_ids || body.zalo_user_ids.length === 0) {
         return res.status(400).json({ message: 'zalo_user_ids is required' })
       }
@@ -12,7 +14,7 @@ const zaloController = {
           'templateId': '00126fd75392bacce383',
           'templateData': {
             "buttonText": body.button_text,
-            "buttonUrl": "https://zalo.me/s/4220846912575584926/" + body.deep_link,
+            "buttonUrl": `https://zalo.me/s/${MINI_APP_ID}/` + body.deep_link,
             "title": body.title,
             "contentTitle": body.content_title,
             "contentDescription": body.content_description
@@ -20,7 +22,7 @@ const zaloController = {
         }, {
           headers: {
             'X-Api-Key': 'Bearer gk3f2MNF41Ub-Ee5PiuK3Q7vvHvWo1q1hllZ4JJs0SWcPD0N60',
-            'X-MiniApp-Id': '4220846912575584926',
+            'X-MiniApp-Id': `${MINI_APP_ID}`,
             'X-User-Id': id
           }
         })

8ce70599015a8ba8b8f3a17314a522850478105f
Nguyen Toan <ngoctoan06011998>
2025-01-09 09:49:08 +0700

    create: recaptchaMiddleware

--- routes/bannersRoute.js
+++ routes/bannersRoute.js
@@ -1,11 +1,13 @@
 const route = require("express").Router()
 const bannersController = require("../controllers/bannersController");
+const recaptchaMiddleware = require("../middleware/recaptcha.middleware")
 
 
-route.post("/", bannersController.addBanner)
-route.get("/", bannersController.getAllBanner)
-route.get("/:id", bannersController.getBannerById)
-route.put("/:id", bannersController.updateBannerById)
-route.delete("/:id", bannersController.deleteBannerById)
+route
+  .post("/", bannersController.addBanner)
+  .get("/", recaptchaMiddleware.verify, bannersController.getAllBanner)
+  .get("/:id", bannersController.getBannerById)
+  .put("/:id", bannersController.updateBannerById)
+  .delete("/:id", bannersController.deleteBannerById)
 
 module.exports = route

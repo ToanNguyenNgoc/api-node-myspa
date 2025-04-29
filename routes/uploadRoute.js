1b44e5fa172ff89dda298c4d769e8b83c32b9b5c
Nguyen Toan <ngoctoan06011998>
2025-04-29 10:25:15 +0700

    unauth media

--- routes/uploadRoute.js
+++ routes/uploadRoute.js
@@ -3,7 +3,7 @@ const { upload, uploadCloud } = require('../middleware/multerMiddle')
 const uploadController = require('../controllers/uploadController')
 const authMiddle = require('../middleware/authMiddle')
 
-route.post('/media', authMiddle.verifyToken, upload.single('file'), uploadController.media);
+route.post('/media', upload.single('file'), uploadController.media);
 route.post('/media_multiple', authMiddle.verifyToken, upload.array('files', 10), uploadController.media_multiple)
 route.post('/cloudinary', authMiddle.verifyToken, uploadCloud.single('file'), uploadController.up_cloudinary)
 
